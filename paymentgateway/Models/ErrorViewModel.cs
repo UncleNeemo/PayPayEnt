96c87a7a727bb0036878c5f5ba9700ac6f5d0dd8
UncleNeemo <102915160+UncleNeemo@users.noreply.github.com>
2023-01-14 14:35:49 -0800

    Delete paymentgateway directory

--- paymentgateway/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace paymentgateway.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
