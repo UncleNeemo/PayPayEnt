1666c7cb3ddbdc00c7c2f338e51b20aafa3f4779
UncleNeemo <102915160+UncleNeemo@users.noreply.github.com>
2023-01-14 14:31:45 -0800

    Add files via upload

---
+++ paymentgateway/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace paymentgateway.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
